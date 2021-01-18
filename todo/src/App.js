4fa2069bb86431bb39104c41943317345a0defba
taratimmerman <timmermantara@gmail.com>
2021-01-18 16:31:25 -0700

    created reducer & actions

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,25 @@
 import React, { useReducer } from 'react';
 import reducer from './reducers/todoReducer';
 import './App.css';
+import { addTask, markDone, clearDone } from './actions/todoActions';
+
+export const initialState = [];
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setTask = (task) => {
+    dispatch(addTask(task))
+  }
+
+  const finishTask = (taskId) => {
+    dispatch(markDone(taskId))
+  }
+
+  const clearCompleted = () => {
+    dispatch(clearDone())
+  }
+
   return (
     <div className="App">
 
